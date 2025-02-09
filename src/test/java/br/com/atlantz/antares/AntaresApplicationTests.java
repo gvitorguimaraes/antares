bedd440ff69757364a3886d5a2653f1d491a841e
Gabriel Guimaraes <gabriel1.vlguimaraes@gmail.com>
2025-02-09 11:01:18 -0300

    default project setup

---
+++ src/test/java/br/com/atlantz/antares/AntaresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atlantz.antares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
